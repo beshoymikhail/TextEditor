b635bd6033c0f35d2c2ca9298f230892daa0e012
beshoy <bbeshoymikhail@gmail.com>
2024-02-15 17:22:29 +0200

    #Adding new components for buttons

---
+++ Pages/NewFile.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace TextEditor.Pages
+{
+    public partial class NewFile
+    {
+        [Parameter]
+        public bool IsFileUploaded { get; set; }
+    }
+}
