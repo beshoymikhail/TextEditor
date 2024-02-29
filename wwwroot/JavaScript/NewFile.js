4958a991faa988c706b13473e95620a337612a1f
beshoymikhail <bbeshoymikhail@gmail.com>
2024-02-29 16:32:36 +0200

    Finsh First Demo

--- wwwroot/JavaScript/NewFile.js
+++ wwwroot/JavaScript/NewFile.js
@@ -9,9 +9,10 @@ function ReplaceFile(InputId) {
 function DeleteFile(InputId) {
     document.getElementById(InputId).DeleteFile();
 }
-$(document).ready(function () {
-    $('.nav-link').on('click', function () {
-        $('.nav-link').removeClass('active');
-        $(this).addClass('active');
-    });
-})
+
+function renderjQueryComponents() {
+    $("#btn-open-existing-file").on('click', function () {''
+
+        $("#open-existing-file").click()
+    })
+}
